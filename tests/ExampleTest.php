d0d2e0dc27038ef9ca9e2dbdd4420b660a0b3174
“GabrielMoreno” <gabrieljmorenot@gmail.com>
2017-05-23 15:31:10 -0400

    Configuracion del entorno de pruebas

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,6 +6,10 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    // usamos el trait DatabaseTransactions para que se eejcute dentro de una transaccion
+    // y que la db siempre este vacia
+    use DatabaseTransactions;
+
     /**
      * A basic functional test example.
      *
@@ -13,7 +17,14 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        // creamos un usuario
+        $user = factory(\App\User::class)->create([
+            'name' => 'Gabriel Moreno',
+            'email' => 'gabrieljmorenot@gmail.com'
+        ]);
+        // iniciamos sesion pasandole el usuario creado y como 2do param el driver por el cual quiero autenticar
+        $this->actingAs($user, 'api')
+            ->visit('api/user')
+             ->see('Gabriel Moreno gabrieljmorenot@gmail.com');
     }
 }
