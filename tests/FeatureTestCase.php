e85bfe6ae317cfb7075a7ba8431111ac36e40e76
“GabrielMoreno” <gabrieljmorenot@gmail.com>
2017-05-25 15:14:52 -0400

    Personalizar el directorio tests de tu aplicacion

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -5,6 +5,6 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class FeatureTestCase extends TestCase
 {
     // usamos el trait DatabaseTransactions para que se eejcute dentro de una transaccion
-    // y que la db siempre este vacia
+    // y que la db siempre este vacia.
     use DatabaseTransactions;
 }
