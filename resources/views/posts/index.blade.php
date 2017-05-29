02c0ce07175bf182030d6bf2797191b182ee3f89
“GabrielMoreno” <gabrieljmorenot@gmail.com>
2017-05-29 14:15:14 -0400

    Paginar registros con TDD.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,4 +10,6 @@
             </li>
         @endforeach
     </ul>
+
+    {{ $posts->render() }}
 @endsection
--- tests/features/PostsListTest.php
+++ tests/features/PostsListTest.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Post;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PostsListTest extends FeatureTestCase
 {
-    public function test_a_user_can_see_the_posts_list_and_go_to_the_detalis()
+    function test_a_user_can_see_the_posts_list_and_go_to_the_detalis()
     {
         /** Having (Teniendo) **/
         // creamos un post
@@ -25,5 +27,37 @@ class PostsListTest extends FeatureTestCase
             ->click($post->title)
             // al hacer click nos llevará a al post
             ->seePageIs($post->url);
+    }
+
+    function test_the_posts_are_paginated()
+    {
+        /** Having (Teniendo) **/
+        // creamos un post personalizado
+        $first = factory(Post::class)->create([
+            'title' => 'Post más antiguo',
+            // simulamos que este post fue creado hace 2 dias
+            'created_at' => Carbon::now()->subDays(2),
+        ]);
+
+        // generamos unos 15 posts con el metodo times()
+        factory(Post::class)->times(15)->create([
+            // simulamos que este post fue creado ayer
+            'created_at' => Carbon::now()->subDay(),
+        ]);
+
+        // creamos un post personalizado
+        $last = factory(Post::class)->create([
+            'title' => 'Post más reciente',
+            // simulamos que este post fue creado hoy
+            'created_at' => Carbon::now(),
+        ]);
+
+        /** When (Cuando) **/
+        $this->visit('/')
+            ->see($last->title)
+            ->dontSee($first->title)
+            ->click('2')
+            ->see($first->title)
+            ->dontSee($last->title);
     }
 }
